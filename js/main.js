2f79573cb97797d44f035a6401603299eba84845
YUMEE <yumeehk@hotmail.com>
2018-07-17 14:38:42 -0400

    Lightswitch works

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+var background = document.querySelector('.light');
+var lightSwitch = document.querySelector('.switch.on');
+var text = document.querySelector('.status');
 
+lightSwitch.addEventListener('click', function() {
+  background.classList.toggle('dark');
+  lightSwitch.classList.toggle('off');
 
+  if (lightSwitch.classList.contains('off')) {
+    text.innerHTML = "Hey, who turned off the lights?";
+  }
+  else {
+    text.innerHTML = "It's so bright in here!";
+  }
+});
